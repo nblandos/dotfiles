115fe19a4b293d4f43c93b611eb059031bce6d8c
nblandos <nblandos@gmail.com>
2025-03-16 15:28:47 +0000

    add yazi config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -71,5 +71,15 @@ if status is-interactive; and test "$TERM_PROGRAM" != vscode
     fastfetch
 end
 
+# Yazi wrapper
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 # Initialise zoxide
 zoxide init fish | source
