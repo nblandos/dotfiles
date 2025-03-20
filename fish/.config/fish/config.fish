a6fbdb2a7a47b8f8d99abd77feb990ab3f24ecb0
nblandos <nblandos@gmail.com>
2025-03-20 14:19:05 +0000

    add copilot toggle in nvim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,7 @@
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx BROWSER wslview
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
